99ccf4fa894954f1ab704a5dda62cba651a67681
Andrew Miller <socrates1024@gmail.com>
2024-09-15 16:44:30 -0400

    fix

---
+++ README.md
@@ -0,0 +1,30 @@
+# Dstack as a VM
+
+## Building the VM image
+
+It's not necessary to have TDX, since the we support running in a mock environment. However, it's necessary to install qemu, and libvirt.
+
+```
+apt-get install qemu qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils virt-manager
+```
+
+Have to start by getting the image
+```bash
+wget https://cloud-images.ubuntu.com/minimal/releases/noble/release-20240903/ubuntu-24.04-minimal-cloudimg-amd64.img
+```
+
+Then run
+```bash
+./build_vm.sh
+```
+
+It's convenient when developing to take a copy of the image after `apt get update`
+
+## Helping other nodes join the network
+
+The enclave is capable of helping others join the network, but the host has to manage this.
+
+The provided host script monitors the Sepolia blockchain, looking for requests.
+
+Additionally 
+
--- build_vm.sh
+++ build_vm.sh
@@ -1,26 +1,17 @@
 set -ex
 
-# This is a sequence of libvirt commands.
-# Saving the intermediate state after packages is meant to
-# reduce the rebuild time, without using overlays.
-#   Wish this were more like dockerfiles!
-#   So instead it's only practical with a couple cache points
-
-# Step 1
-if false; then
-    qemu-img convert -f qcow2 ./ubuntu-minimal.img -O qcow2 ubuntu_vm.img
-    virt-customize -a ubuntu_vm.img \
-      --update \
-      --install cryptsetup,wget,python3-pip,isc-dhcp-client \
-      --install pipx,python3-nacl,python3-flask,python3-requests \
-      --run-command 'pip install environs eth_account --break-system-packages' \
-      --copy-in foundry_nightly_linux_amd64.tar.gz:/root \
-      --run-command 'tar -xzf /root/foundry_nightly_linux_amd64.tar.gz -C /usr/local/bin' \
-      --copy-in vm_files/startup_script.sh:/usr/local/bin/ \
-      --copy-in vm_files/startup_script.service:/etc/systemd/system/ \
-      --run-command 'chmod +x /usr/local/bin/startup_script.sh' \
-      --run-command 'systemctl enable startup_script.service' \
-      --copy-in vm_files/guest_service.py:/root \
-      --copy-in vm_files/register.py:/root \
-      --copy-in vm_files/app.py:/root
-fi
+qemu-img convert -f qcow2 ./ubuntu-minimal.img -O qcow2 ubuntu_vm.img
+virt-customize -a ubuntu_vm.img \
+   --update \
+   --install cryptsetup,wget,python3-pip,isc-dhcp-client \
+   --install pipx,python3-nacl,python3-flask,python3-requests \
+   --run-command 'pip install environs eth_account --break-system-packages' \
+   --copy-in foundry_nightly_linux_amd64.tar.gz:/root \
+   --run-command 'tar -xzf /root/foundry_nightly_linux_amd64.tar.gz -C /usr/local/bin' \
+   --copy-in vm_files/startup_script.sh:/usr/local/bin/ \
+   --copy-in vm_files/startup_script.service:/etc/systemd/system/ \
+   --run-command 'chmod +x /usr/local/bin/startup_script.sh' \
+   --run-command 'systemctl enable startup_script.service' \
+   --copy-in vm_files/guest_service.py:/root \
+   --copy-in vm_files/register.py:/root \
+   --copy-in vm_files/app.py:/root
