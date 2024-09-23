d9b9e9641a8c2ef7cb11910f1f88ee3218104c0f
Andrew Miller <socrates1024@gmail.com>
2024-09-22 22:22:14 -0400

    All the domain handling moved to unstoppable_tls

--- vm_files/app.py
+++ vm_files/app.py
@@ -1,12 +1,10 @@
 # Sample application
-import os, hashlib, time
 from unstoppable_tls import give_me_the_keys
 from flask import Flask
 import requests
-import tempfile
-from cryptography import x509
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import serialization
+import sys
+cli = sys.modules['flask.cli']
+cli.show_server_banner = lambda *x: None
 
 app = Flask(__name__)
 
@@ -14,37 +12,6 @@ app = Flask(__name__)
 def home():
     return "Hello from Dstack TLS!"
 
-CERTIFICATE_PATH = "/mnt/host_volume/certificate.pem"
-
 if __name__ == '__main__':
-    pem_key = give_me_the_keys()
-    fp = tempfile.NamedTemporaryFile(delete_on_close=False)
-    fp.write(pem_key)
-    fp.close()
-
-    print('waiting for certificate.pem...', end='', flush=True)
-    while not os.path.isfile(CERTIFICATE_PATH):
-        print('.', end='', flush=True)
-        time.sleep(2)
-    print()
-    time.sleep(0.2)
-
-    cert_data = open(CERTIFICATE_PATH,'rb').read()
-    cert = x509.load_pem_x509_certificate(cert_data, default_backend())
-    public_key = cert.public_key()
-    public_bytes = public_key.public_bytes(
-        encoding=serialization.Encoding.X962,
-        format=serialization.PublicFormat.UncompressedPoint
-    )
-    print('pubkey:', public_bytes.hex())
-
-    tag = 'unstoppable_tls'
-    app_data = public_bytes.hex()
-    url = f"http://localhost:4001/attest/{tag}/{app_data}"
-    resp = requests.get(url)
-    sig = resp.content
-    print('sig:', sig.hex())
-
-    app.run(ssl_context=(CERTIFICATE_PATH,
-                         fp.name),
-            port=4002, host='0.0.0.0')
+    key_path, cert_path = give_me_the_keys()
+    app.run(host='0.0.0.0', port=4002, ssl_context=(cert_path, key_path))
--- vm_files/unstoppable_tls.py
+++ vm_files/unstoppable_tls.py
@@ -1,4 +1,4 @@
-import os
+import os, time
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from flask import Flask
 import requests
@@ -9,9 +9,11 @@ from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.primitives.hashes import SHA256
+import tempfile
 
 DOMAIN_NAME = "dstack-mockup.ln.soc1024.com"
 CSR_PATH = "/mnt/host_volume/request.csr"
+CERTIFICATE_PATH = "/mnt/host_volume/certificate.pem"
 
 def get_private_key():
     # Fetch a deterministic 32-byte key from the pseudorandom service
@@ -29,10 +31,40 @@ def generate_csr(private_key):
 
 def give_me_the_keys():
     private_key = get_private_key()
+
+    # Create a signature on the public key
+    public_key = private_key.public_key()
+    public_bytes = public_key.public_bytes(
+        encoding=serialization.Encoding.X962,
+        format=serialization.PublicFormat.UncompressedPoint
+    )
+    print('pubkey:', public_bytes.hex())
+    tag = 'unstoppable_tls'
+    app_data = public_bytes.hex()
+    url = f"http://localhost:4001/attest/{tag}/{app_data}"
+    resp = requests.get(url)
+    sig = resp.content
+    print('sig:', sig.hex())
+
+    # Write a CSR just in case we need it
     generate_csr(private_key)
+
+    # Wait for the certificate
+    print('waiting for certificate.pem...', end='', flush=True)
+    while not os.path.isfile(CERTIFICATE_PATH):
+        print('.', end='', flush=True)
+        time.sleep(2)
+    print()
+    time.sleep(0.1)
+
+    # Write the private key to temp dir
+    fp = tempfile.NamedTemporaryFile(delete=False)
     pem_key = private_key.private_bytes(
         encoding=serialization.Encoding.PEM,
         format=serialization.PrivateFormat.PKCS8,
         encryption_algorithm=serialization.NoEncryption(),
     )
-    return pem_key
+    fp.write(pem_key)
+    fp.close()
+
+    return fp.name, CERTIFICATE_PATH
